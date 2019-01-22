3a3a2b607b5760067a49bf09a391a2ebf64e4ba4
Maitrayan Ghosh <46134007+maitrayanGhosh@users.noreply.github.com>
2019-01-23 01:53:02 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,87 +1,3 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//class Node{
-//public:
-//    int data;
-//    Node *left;
-//    Node *right;
-//};
-//
-//Node *insert(Node *root, int val);
-//Node *Create(int val);
-//void inorder(Node *root);
-//void preorder(Node *root);
-//
-//
-//
-//int main(){
-//    Node *root=NULL;
-////
-////    int n;
-////    cin>>n;
-////
-////    for(int i=0;i<n;i++){
-////        int temp;
-////        cin>>temp;
-////        root=insert(root,temp);
-////    }
-//
-//     root=insert(root,15);
-//
-//     root=insert(root,10);
-//
-//     root=insert(root,20);
-//     root=insert(root,25);
-//
-//     root=insert(root,12);
-//
-//
-//    inorder(root);
-//    cout<<endl;
-//    preorder(root);
-//}
-//
-//Node *Create(int val){// this return a address of node which is of type node
-//    Node *newNode = new Node();
-//    newNode->data=val;
-//    newNode->left=NULL;
-//    newNode->right=NULL;
-//    return newNode;
-//}
-//
-//Node *insert(Node *root , int val){// return type is pointer to node(return address of node created)
-//
-//    if(root==NULL){
-//        root = Create(val);
-//    }
-//    else if(val <= root->data){
-//        root->left=insert(root->left,val);
-//    }
-//    else{
-//        root->right=insert(root->right,val);
-//    }
-//
-//    return root;
-//}
-//void inorder(Node *root){
-//    if(root==NULL){
-//        return;
-//    }
-//    inorder(root->left);
-//    cout<<root->data<<" ";
-//    inorder(root->right);
-//}
-//
-//void preorder(Node *root){
-//    if(root==NULL){
-//        return;
-//    }
-//    cout<<root->data<<" ";
-//    preorder(root->left);
-//    preorder(root->right);
-//}
 
 
 #include <iostream>
@@ -102,8 +18,12 @@ public:
 };
 
 Node *insert(Node *root , int val);
-void inorder(Node* root);
-//Node *Create(int val);
+void inorder(Node *root);
+void preorder(Node *root);
+void postorder(Node *root);
+
+
+Node *Create(int val);
 int main() {
 
 
@@ -148,11 +68,33 @@ void inorder(Node* root){
         inorder(root->right);
     }
 }
-//
-//Node *Create(int val) {// this return a address of node which is of type node
-//    Node *newNode = new Node();
-//    newNode->data = val;
-//    newNode->left = NULL;
-//    newNode->right = NULL;
-//    return newNode;
-//}
+
+void postorder(Node *root){
+    if(root==NULL){
+        return;
+    }
+    else{
+        postorder(root->left);
+        postorder(root->right);
+        cout<<root->data<<endl;
+    }
+}
+
+
+Node *Create(int val){// this return a address of node which is of type node
+    Node *newNode = new Node();
+    newNode->data=val;
+    newNode->left=NULL;
+    newNode->right=NULL;
+    return newNode;
+}
+
+
+void preorder(Node *root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
